1e76bf7c168cc13ea537d57a311bad85f8402a10
CamronCbell <campbell_camron@students.columbusstate.edu>
2026-01-30 13:51:00 -0500

    Add files via upload

---
+++ First.java
@@ -0,0 +1,11 @@
+//Name: Camron Campbell
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Job title: Software developer");
+		System.out.println("Why: I enjoy programming and good money");
+		System.out.println("Median Salary: 131,450");
+	}
+}
