b3ffae7f8f7c33dd8effc8404bdd865bd6d79b84
CamronCbell <campbell_camron@students.columbusstate.edu>
2026-02-21 14:35:27 -0500

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,32 +1,11 @@
-import java.util.Scanner;
-
 public class Runner {
 
-    public static void main(String[] args) 
-    {
-        Pet myPet1 = new Pet();
-        System.out.println(myPet1);
+    public static void main(String[] args) {
 
-        Pet myPet2 = new Pet("Cat", "Sakura", 4);
-        System.out.println(myPet2);
+        Tests myTests = new Tests();
 
-        Scanner input = new Scanner(System.in);
+        myTests.getAverage();
 
-        System.out.println("Enter animal type:");
-        String type = input.nextLine();
-
-        System.out.println("Enter animal name:");
-        String name = input.nextLine();
-
-        System.out.println("Enter animal age:");
-        int age = input.nextInt();
-
-        Pet myPet3 = new Pet(type, name, age);
-
-        System.out.println();
-        System.out.println(myPet3);
-
-        input.close();
+        System.out.println(myTests.toString());
     }
 }
-
--- Tests.java
+++ Tests.java
@@ -1,19 +1,45 @@
-//Name: Camron Campbell
+import java.util.Scanner;
+
 public class Tests 
 {
 
-	public static void main(String[] args)
-	{
-	double Test1 = 92.7;
-	double Test2 = 87.9;
-	double Test3 = 79.6;
-	double total = Test1 + Test2 + Test3;
-	double average = total/3;
-	System.out.println("Test score 1: " + Test1);
-	System.out.println("Test score 2: " + Test2);
-	System.out.println("Test score 3: " + Test3);
-	System.out.print("The average of 3 test scores is: ");
-	System.out.format("%.2f%n",average);
-	}
+    private double average;
+    private int count;
 
+    public Tests() 
+    {
+        average = 0;
+        count = 0;
+    }
+
+    public void getAverage() 
+    {
+
+        Scanner scan = new Scanner(System.in);
+
+        double sum = 0.0;
+        count = 0;
+
+        System.out.println("Enter test scores or -1 to quit");
+        double score = scan.nextDouble();
+
+        while (score != -1) {
+            sum += score;
+            count++;
+
+            System.out.println("Enter test scores or -1 to quit");
+            score = scan.nextDouble();
+        }
+
+        if (count > 0) {
+            average = sum / count;
+        } else {
+            average = Double.NaN;
+        }
+    }
+
+    // toString method
+    public String toString() {
+        return "The average of the " + count + " scores entered is " + String.format("%.2f", average) + ".";
+    }
 }
