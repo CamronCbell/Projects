0b03a210b6a3e4aad8f8f3d9f3c274c113e498db
CamronCbell <campbell_camron@students.columbusstate.edu>
2026-05-04 12:58:14 -0400

    Add files via upload

---
+++ TowerRunner.java
@@ -0,0 +1,9 @@
+
+public class TowerRunner {
+	//Instantiate a Towers object to swap the rings on 3 "towers"
+	public static void main (String[] args)
+	{
+		Tower towersOfHanoi = new Tower(3);
+	}
+
+}
